e6a6c05d0d161dc72ef035a59a1ac32c2fc5fe62
Daniel Cousens <github@dcousens.com>
2017-05-23 15:23:04 +1000

    safe-buffer

--- make-hash.js
+++ make-hash.js
@@ -1,19 +1,19 @@
 'use strict'
-var intSize = 4
-var zeroBuffer = new Buffer(intSize)
-zeroBuffer.fill(0)
+var INT32_SIZE = 4
+var CHAR64_SIZE = 8
+var HASH128_SIZE = 16
 
-var charSize = 8
-var hashSize = 16
+var Buffer = require('safe-buffer').Buffer
+var zeroBuffer = Buffer.alloc(INT32_SIZE, 0)
 
-function toArray (buf) {
-  if ((buf.length % intSize) !== 0) {
-    var len = buf.length + (intSize - (buf.length % intSize))
+function asUInt32Array (buf) {
+  if ((buf.length % INT32_SIZE) !== 0) {
+    var len = buf.length + (INT32_SIZE - (buf.length % INT32_SIZE))
     buf = Buffer.concat([buf, zeroBuffer], len)
   }
 
   var arr = new Array(buf.length >>> 2)
-  for (var i = 0, j = 0; i < buf.length; i += intSize, j++) {
+  for (var i = 0, j = 0; i < buf.length; i += INT32_SIZE, j++) {
     arr[j] = buf.readInt32LE(i)
   }
 
@@ -21,10 +21,12 @@ function toArray (buf) {
 }
 
 module.exports = function hash (buf, fn) {
-  var arr = fn(toArray(buf), buf.length * charSize)
-  buf = new Buffer(hashSize)
+  var arr = fn(asUInt32Array(buf), buf.length * CHAR64_SIZE)
+  buf = new Buffer(HASH128_SIZE)
+
   for (var i = 0; i < arr.length; i++) {
     buf.writeInt32LE(arr[i], i << 2, true)
   }
+
   return buf
 }
